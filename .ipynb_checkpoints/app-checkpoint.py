a9abfb000d0c4642b67fc65cc6ec676cbf665d53
anandpal5134-boop <anandpal5134@gmail.com>
2025-08-27 10:58:40 +0530

    Initial Commit 1.1

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,48 @@
+#app.py
+import os
+import json
+import joblib
+import numpy as np
+from flask import Flask,request,jsonify
+
+#--Config--
+MODEL_PATH=os.getenv("MODEL_PATH","model/iris_model.pkl") 
+
+#---App--
+app=Flask(__name__)
+#Load once at startup
+try:
+    model = joblib.load(MODEL_PATH)
+except Exception as e:
+   #fail fast with a helpful message
+    raise RuntimeError(f"Could not load model from {MODEL_PATH}: {e}")
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}, 200
+@app.post("/predict")
+def predict():
+    """Accepts either:
+    {"input":[[.....feature vector....],[...]]}
+    or
+    {"input":[....feature vector....]}"""
+    try:
+        payload=request.get_json(force=True)
+        x=payload.get("input")
+        if x is None:
+            return jsonify(error="Missing 'input'"),400
+        # Normalize to 2D Ar
+        if isinstance(x,list) and (len(x)>0) and not isinstance(x[0],list):
+            x = [x]
+
+        x = np.array(x,dtype=float)
+        preds = model.predict(x)
+        # IF your model returns numpy types, conver to p
+        preds = preds.tolist()
+        return jsonify(prediction=preds),200
+
+    except Exception as e:
+        return jsonify(error=str(e)),500
+if __name__=="__main__":
+    #local dev only; Render will run with Gunicorn (see startcommand below)
+    app.run(host="0.0.0.0",port=int(os.environ.get("PORT",8000)))
